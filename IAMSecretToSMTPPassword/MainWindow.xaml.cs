1acd65a53e3d6bf77191ba1ec76a2931471bd609
Dave Greene <davepgreene@gmail.com>
2014-11-24 09:21:42 -0500

    Initial commit

---
+++ IAMSecretToSMTPPassword/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Windows;
+
+namespace IAMSecretToSMTPPassword
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(this.IAMKey.Text))
+            {
+                MessageBoxResult result = MessageBox.Show("You must enter an IAM key.");
+                return;
+            }
+            //key = AWS Secret Access Key;
+            string IAMKey = this.IAMKey.Text;
+
+            //message = "SendRawEmail";
+            string message = "SendRawEmail";
+            
+            //versionInBytes = 0x02;
+            byte versionInBytes = 0x02;
+            
+            //signatureInBytes = HmacSha256(message, key);
+            byte[] signatureInBytes = HashHMAC(message, IAMKey);
+
+            //signatureAndVer = Concatenate(versionInBytes, signatureInBytes);
+            byte[] signatureAndVer = new byte[signatureInBytes.Length + 1];
+            signatureInBytes.CopyTo(signatureAndVer, 1);
+            signatureAndVer[0] = versionInBytes;
+           
+            //smtpPassword = Base64(signatureAndVer);
+            string smtpPassword = Convert.ToBase64String(signatureAndVer);
+
+            this.SMTPPass.Text = smtpPassword;
+        }
+
+        private byte[] HashHMAC(string message, string secret)
+        {
+            secret = secret ?? "";
+            var encoding = new System.Text.ASCIIEncoding();
+            byte[] keyByte = encoding.GetBytes(secret);
+            byte[] messageBytes = encoding.GetBytes(message);
+            using (var hmacsha256 = new HMACSHA256(keyByte))
+            {
+                byte[] hashmessage = hmacsha256.ComputeHash(messageBytes);
+                return hashmessage;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+﻿## Synopsis
+
+A WPF C# app designed to take an AWS IAM secret key and generate an AWS SES SMTP password.
+
+## Motivation
+
+Amazon Web Services SES (Simple Email Service) requires its own specially generated SMTP password that is derived from the IAM secret key.
+
+See [Obtaining Your Amazon SES SMTP Credentials](https://docs.aws.amazon.com/ses/latest/DeveloperGuide/smtp-credentials.html) in the AWS documentation.
+
+While Amazon doesn't recommend this practice it can be unwieldy to manage two sets of credentials for every IAM user. It's simply easier to generate a second key with only "ses:SendRawEmail" permissions.
+
+## Installation
+
+Build using the Release profile.
+
+## License
+
+Distributed under the Simplified BSD license. See [LICENSE.md](LICENSE.md) for full license text.
